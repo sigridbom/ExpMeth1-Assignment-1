729e42f3273ce1c0ec0f533ab05a28bbf1239dd7
sigridbom <sigridabn@hotmail.com>
2019-09-24 12:11:14 +0200

    exercises

--- Chapter 4.R
+++ Chapter 4.R
@@ -1,6 +1,6 @@
 ####CHAPTER 4 Exploring data with graphs 
 
-install.packages("ggplot2")
+#install.packages("ggplot2")
 library(ggplot2)
 
 personality_test <- read.csv("NEW_CogSciPersonalityTest2019.csv")
@@ -13,3 +13,9 @@ my_graph + opts(title = "Test")
 
 my_graph + geom_bar() 
 ### WHy "must not be used with a y aesthetic???
+
+#setwd("/Users/sigridnielsen/Dropbox/Dokumenter/Uni/1. semester/R studio data/ExpMeth1-Assignment-1")
+#setwd("Users/sigridnielsen/ExpMeth1-Assignment-1/") 
+#don't move files around when R is open - close R and open it again afterwards
+# a trick is to keep a setwd function on the top of the script and then run it everytime you open it
+
